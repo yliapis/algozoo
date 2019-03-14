c26a868be2f255480aa51f9a746eb2ac785208bb
yliapis <yliapis44@gmail.com>
2019-03-13 20:59:37 -0500

    added skeleton for rbtree

---
+++ algozoo/trees/red_black_tree.py
@@ -0,0 +1,15 @@
+
+
+from binary_search_tree import BinarySearchTree
+
+
+def RedBlackTree(BinarySearchTree):
+
+    def __init__(self, *args, **kwargs):
+
+        self.black = True
+
+        super().__init__()
+
+        # remove once implementation is complete
+        raise NotImplementedError

1bd09a26f64828bc1ce0837ac2dd4c25d32807b8
yliapis <yliapis44@gmail.com>
2019-03-14 01:08:07 -0500

    Simplified BST implementation

--- algozoo/trees/binary_search_tree.py
+++ algozoo/trees/binary_search_tree.py
@@ -1,124 +1,149 @@
 
+#: Node def
 
-#: class def
 
+class _TreeNode:
 
-class _BSTNode:
-    """
-    """
-
-    def __init__(self, val=None, left=None, right=None,
-                 comparator=None, *args, **kwargs):
-
+    def __init__(self, val):
         self.val = val
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
 
-        if comparator is None:
-            self.comparator = lambda a, b: a > b
-        else:
-            self.comparator = comparator
 
-        for name, val in kwargs:
-            setattr(self, name, val)
-
-    def __contains__(self, val):
-
-        return self.search(val)
-
-    def __iter__(self):
-
-        if self.left:
-            yield from self.left
-        yield self.val
-        if self.right:
-            yield from self.right
-
-    def __repr__(self):
-
-        repr_vals = ", ".join(map(repr, self))
-        repr_str = "{name}({items})".format(name=self.__class__.__name__,
-                                            items=repr_vals)
-
-        return repr_str
-
-    @classmethod
-    def from_iterable(cls, iterable):
-
-        self = cls()
-        self.insert_items(iterable)
-        return self
-
-    def insert(self, val, *args, **kwargs):
-
-        if self.val is None:  # for uninitialized tree
-            self.val = val
-        elif self.comparator(val, self.val):
-            if self.right:
-                self.right.insert(val)
-            else:
-                self.right = _BSTNode(val, comparator=self.comparator,
-                                      *args, **kwargs)
-        else:
-            if self.left:
-                self.left.insert(val)
-            else:
-                self.left = _BSTNode(val, comparator=self.comparator,
-                                     *args, **kwargs)
-
-    def insert_items(self, items):
-
-        for item in items:
-            self.insert(item)
-
-    def search(self, val):
-
-        if self.val is None:
-            return False
-        if self.val == val:
-            return True
-        elif self.comparator(val, self.val):
-            if self.right:
-                return self.right.search(val)
-            else:
-                return False
-        else:
-            if self.left:
-                return self.left.search(val)
-            else:
-                return False
+#: Tree def
 
 
 class BinarySearchTree:
 
-    def __init__(self, items=None):
+    _node_factory = _TreeNode
+
+    def __init__(self, items=None, comparator=None):
+
+        self.root = None
+        self.size = 0
+        
+        if comparator is None:
+            self.comparator = lambda a, b: a > b
+        elif callable(comparator):
+            self.comparator = comparator
+        else:
+            raise Exception("comparator={!r} not callable".format(comparator))
 
         if hasattr(items, '__iter__'):
-            self.root = _BSTNode.from_iterable(items)
-        elif items is None:
-            self.root = _BSTNode()
-        else:
-            raise Exception("items={} is not iterable".format(items))
+            for item in items:
+                self.insert(item)
+        elif items is not None:
+            raise Exception("items={!r} is not iterable".format(items))
 
     def __contains__(self, val):
-        return val in self.root
+        return self.search(val)
 
-    def __iter__(self):
-        return iter(self.root)
+    def __iter__(self, node=None, start=True):
+        """ DFS iteration """
+
+        if start:
+            node = self.root
+
+        if node:
+            if node.left:
+                yield from self.__iter__(node.left, start=False)
+            yield node.val
+            if node.right:
+                yield from self.__iter__(node.right, start=False)
+
+    def __len__(self):
+        return self.size
 
     def __repr__(self):
 
-        repr_vals = ", ".join(map(repr, self))
-        repr_str = "{name}({items})".format(name=self.__class__.__name__,
-                                            items=repr_vals)
+        bst_str = ', '.join(repr(item) for item in self)
+        bst_str = "{name}({items})".format(name=self.__class__.__name__,
+                                           items=bst_str)
 
-        return repr_str
+        return bst_str
 
-    def insert(self, *args, **kwargs):
-        self.root.insert(*args, **kwargs)
+    def insert(self, val):
+        self._insert_node(self._node_factory(val))
+
+
+    def _insert_node(self, new_node):
+
+        self.size += 1
+
+        if not self.root:
+            self.root = new_node
+            return
+
+        node = self.root
+        while True:
+            if self.comparator(new_node.val, node.val):
+                if node.right:
+                    node = node.right
+                else:
+                    node.right = new_node
+                    return
+            else:
+                if node.left:
+                    node = node.left
+                else:
+                    node.left = new_node
+                    return
+
+    def remove(self, val):
+
+        node = self.root
+        parent = None
+
+        if not node:
+            return
+
+        while node:
+            if node.val == val:
+                break
+            elif self.comparator(val, node.val):
+                if node.right:
+                    parent = node
+                    node = node.right
+                else:
+                    return
+            else:
+                if node.left:
+                    parent = node
+                    node = node.left
+                else:
+                    return
+
+        self.size -= 1
+
+        if parent:
+            # break link
+            if self.comparator(node.val, parent.val):
+                parent.right = None
+            else:
+                parent.left = None
+            # insert subtrees
+            if node.right:
+                self._insert_node(node.right)
+            if node.left:
+                self._insert_node(node.left)
+        else:
+            self.root = node.right
+            if node.left:
+                self._insert_node(node.left)
 
     def search(self, val):
-        self.root.search(val)
+
+        node = self.root
+
+        while node:
+            if node.val == val:
+                return True
+            elif self.comparator(val, node.val):
+                node = node.right
+            else:
+                node = node.left
+
+        return False
 
 
 #: for testing
@@ -128,10 +153,15 @@ def _main():
 
     print()
 
-    items = [1, 3, 7, 5, 4]
+    items = [1, 3, 2, 7, 4]
 
     tree = BinarySearchTree(items)
 
+    print(tree)
+    print()
+
+    print("Removing 3")
+    tree.remove(3)
     print(tree)
     print()
 
@@ -140,8 +170,6 @@ def _main():
         print("{i} is {msg}in tree".format(i=i, msg=msg))
 
     print()
-
-    return tree
 
 
 if __name__ == '__main__':

4fac38868b6f600836f1eb093a2b12419670743f
yliapis <yliapis44@gmail.com>
2019-03-13 20:57:49 -0500

    Added some details of implementation

--- algozoo/trees/avl_tree.py
+++ algozoo/trees/avl_tree.py
@@ -5,4 +5,18 @@ from binary_search_tree import BinarySearchTree
 
 def AVLTree(BinarySearchTree):
 
-    pass
+    def __init__(self, *args, **kwargs):
+
+        self.n_left = 0
+        self.n_right = 0
+
+        super().__init__()
+
+        # remove once implementation is complete
+        raise NotImplementedError
+
+
+    @property
+    def balance(self):
+        return self.n_left - self.n_right
+    

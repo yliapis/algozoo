0e7af2b65446a55fa16ac1c873b7b822d8d915da
yliapis <yliapis44@gmail.com>
2019-03-13 21:53:59 -0500

    updated bst

--- algozoo/trees/binary_search_tree.py
+++ algozoo/trees/binary_search_tree.py
@@ -3,21 +3,24 @@
 #: class def
 
 
-class BinarySearchTree:
+class _BSTNode:
     """
     """
 
     def __init__(self, val=None, left=None, right=None,
-                 comparator=None):
+                 comparator=None, *args, **kwargs):
 
         self.val = val
         self.left = left
         self.right = right
 
-        if not comparator:
+        if comparator is None:
             self.comparator = lambda a, b: a > b
         else:
             self.comparator = comparator
+
+        # for name, val in kwargs:
+        #     setattr(self, name, val)
 
     def __contains__(self, val):
 
@@ -43,9 +46,7 @@ class BinarySearchTree:
     def from_iterable(cls, iterable):
 
         self = cls()
-        for val in iterable:
-            self.insert(val)
-
+        self.insert_items(iterable)
         return self
 
     def insert(self, val):
@@ -56,17 +57,22 @@ class BinarySearchTree:
             if self.right:
                 self.right.insert(val)
             else:
-                self.right = BinarySearchTree(
-                                val, comparator=self.comparator)
+                self.right = _BSTNode(val, comparator=self.comparator)
         else:
             if self.left:
                 self.left.insert(val)
             else:
-                self.left = BinarySearchTree(
-                                val, comparator=self.comparator)
+                self.left = _BSTNode(val, comparator=self.comparator)
+
+    def insert_items(self, items):
+
+        for item in items:
+            self.insert(item)
 
     def search(self, val):
 
+        if self.val is None:
+            return False
         if self.val == val:
             return True
         elif self.comparator(val, self.val):
@@ -81,6 +87,38 @@ class BinarySearchTree:
                 return False
 
 
+class BinarySearchTree:
+
+    def __init__(self, items=None):
+
+        if hasattr(items, '__iter__'):
+            self.tree = _BSTNode.from_iterable(items)
+        elif items is None:
+            self.tree = _BSTNode()
+        else:
+            raise Exception("items={} is not iterable".format(items))
+
+    def __contains__(self, val):
+        return val in self.tree
+
+    def __iter__(self):
+        return iter(self.tree)
+
+    def __repr__(self):
+
+        repr_vals = ", ".join(map(repr, self))
+        repr_str = "{name}({items})".format(name=self.__class__.__name__,
+                                            items=repr_vals)
+
+        return repr_str
+
+    def insert(self, *args, **kwargs):
+        self.tree.insert(*args, **kwargs)
+
+    def search(self, *args, **kwargs):
+        self.tree.search(*args, **kwargs)
+
+
 #: for testing
 
 
@@ -90,7 +128,7 @@ def _main():
 
     items = [1, 3, 7, 5, 4]
 
-    tree = BinarySearchTree.from_iterable(items)
+    tree = BinarySearchTree(items)
 
     print(tree)
     print()

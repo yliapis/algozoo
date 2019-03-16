590882e786936a84971df0ce5de78298cc5d5a10
yliapis <yliapis44@gmail.com>
2019-03-15 23:37:14 -0500

    moved heap.py, added API skeleton

---
+++ algozoo/containers/heap.py
@@ -0,0 +1,24 @@
+
+
+class Heap(object):
+
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __contains__(self):
+        pass
+
+    def insert(self, val):
+        pass
+
+    def delete(self, val):
+        pass

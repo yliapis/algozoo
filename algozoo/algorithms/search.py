cc46c2a0ffce63220ce2db212d50c205b8adcbfd
yliapis <yliapis44@gmail.com>
2019-03-16 14:22:06 -0500

    added tests

--- algozoo/algorithms/search.py
+++ algozoo/algorithms/search.py
@@ -1,5 +1,10 @@
 
 
+# testing imports
+if __name__ == '__main__':
+    import random
+
+
 #: search functions
 
 
@@ -8,7 +13,7 @@ def dfs_iter(root=None,
              key=lambda node: node.next):
     """Generic depth-first search iterator
 
-    NOTE: assumes structure is directed acyclic graph;
+    NOTE: assumes structure is tree-like;
     does not work with cyclic data structures
     """
 
@@ -16,7 +21,8 @@ def dfs_iter(root=None,
         return
 
     yield value(root)
-    yield from key(root)
+    for node in key(root):
+        yield from dfs_iter(node)
 
 
 def bfs_iter(root=None,
@@ -24,7 +30,7 @@ def bfs_iter(root=None,
              key=lambda node: node.next):
     """Generic breadth-first search iterator
 
-    NOTE: assumes structure is directed acyclic graph;
+    NOTE: assumes structure is tree-like;
     does not work with cyclic data structures
     """
 
@@ -34,28 +40,63 @@ def bfs_iter(root=None,
         node = queue.pop(0)
         if node is not None:
             yield value(node)
-            queue.extend(list(key(node)))
+            queue.extend(key(node))
 
 
 #: for testing
 
 
-def _test_dfs():
-    pass
+class __testNode:
+
+    def __init__(self, val, next=None):
+
+        self.val = val
+
+        if next is None:
+            self.next = []
+        else:
+            self.next = next
+
+    def __repr__(self):
+        return "({val}: ({next}))".format(
+                    val=self.val,
+                    next=", ".join(map(repr, self.next)))
 
 
-def _test_bfs():
-    pass
+def __construct_random_tree(size=8):
+
+    # create nodes
+    nodes = []
+    for i in range(size):
+        nodes.append(__testNode(i))
+
+    for i in range(1, size):
+        parent_id = random.randint(0, i - 1)
+        nodes[parent_id].next.append(nodes[i])
+
+    return nodes[0]
+
+
+def _test_dfs(tree):
+    print(list(dfs_iter(tree)))
+
+
+def _test_bfs(tree):
+    print(list(bfs_iter(tree)))
 
 
 def _main():
 
     print()
 
-    _test_dfs()
+    tree = __construct_random_tree(8)
+    print(tree)
     print()
 
-    _test_bfs()
+    _test_dfs(tree)
+    print()
+
+    _test_bfs(tree)
     print()
 
 

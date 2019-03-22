9846b89a9ef7433f6329eeeff5fd45f84feeefc6
yliapis <yliapis44@gmail.com>
2019-03-21 20:25:53 -0500

    implemented constructor

--- algozoo/containers/kd_tree.py
+++ algozoo/containers/kd_tree.py
@@ -1,14 +1,30 @@
 
 
+#: helper class
+
+
+class _KDNode:
+
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+#: implementation class
+
+
 class KDTree:
 
     def __init__(self, items):
-
-        raise NotImplementedError
-
         # NOTE: items need to all have the same dimention
-        self.dim = None
-        self.size = None
+        items = list(items)
+        dim = len(items[0])
+
+        self.dim = dim
+        self.size = len(items)
+
+        self.root = self._construct_tree(items)
 
     def __contains__(self, item):
         raise NotImplementedError
@@ -24,6 +40,25 @@ class KDTree:
 
     def __str__(self):
         raise NotImplementedError
+
+    def _construct_tree(self, items, level=0):
+
+        if len(items) == 0:
+            return None
+
+        items = sorted(items, key=lambda item: item[level])
+
+        median = len(items) // 2
+        node = _KDNode(items[median])
+
+        lower = items[:median]
+        upper = items[median + 1:]
+
+        next_level = (level + 1) % self.dim
+        node.left = self._construct_tree(lower, next_level)
+        node.right = self._construct_tree(upper, next_level)
+
+        return node
 
     def add(self, item):
         raise NotImplementedError
